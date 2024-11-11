ef7c2e0d9d306e5fba0677ad1289f24c5c9e049f
triphuc186 <117024721+triphuc186@users.noreply.github.com>
2024-11-11 20:37:35 +0700

    Add files via upload

---
+++ project1-main/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HMS Home Page</title>
+</head>
+<body>
+<?php 
+	include("include/header.php");
+?>
+<div style="margin-top: 50px"></div>
+
+<div class="container">
+	<div class="col-md-12">
+		<div class="row">
+			<div class= "col-md-3 mx-1 shadow">
+				<img src="img/info_1.jpg" style="width: 100%;">
+				<h5 class="text-center">Click on the button for more information</h5>
+				<a href="#">
+					<button class ="btn btn-success my-3" style="margin-left: 35%;">More</button>
+				</a>
+				</div>
+			<div class= "col-md-3 mx-1 shadow">
+				<img src="img/doctor.jpg" style="width: 100%;">
+				<h5 class="text-center">Create Account so that we can take good care of you.</h5>
+				<a href="#">
+					<button class ="btn btn-success my-3" style="margin-left: 30%;">Create Account</button>
+				</a>
+
+				</div>
+			<div class= "col-md-3 mx-1 shadow">
+				<img src="img/patient_1.jpg" style="width: 100%;">
+				<h5 class="text-center">We are employing fors doctors</h5>
+
+				<a href="#">
+					<button class ="btn btn-success my-3" style="margin-left: 30%;">Apply now</button>
+				</a>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
